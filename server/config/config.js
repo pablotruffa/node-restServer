94927d33192e390af34b77ad83fd0b90fc7d0e72
Pablo Truffa <truffa12@gmail.com>
2020-10-19 20:35:49 -0300

    se protegio la variable de entorno en config.js

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV == 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cosme_fulanito:5jpN34XTSHzbFgsK@cluster0.bolt1.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
